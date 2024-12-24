7b6c2145d81f27001ef62c47916bb8aac2c9410f
Code_NooB <alexangel77719@gmail.com>
2024-12-24 17:53:20 +0200

    Fix Routing Path + import

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
-import RecurrenceRelations from './pages/RecurrenceRelations';
-import SolvingRecurrenceRelations from './pages/SolvingRecurrenceRelations';
+// import RecurrenceRelations from './pages/RecurrenceRelations';
+// import SolvingRecurrenceRelations from './pages/SolvingRecurrenceRelations';
 import Sets from './pages/Sets';
 import DecryptMessage from "./pages/DecryptMessage";
 import HammingCode from "./pages/HammingCode";
+import ValidateChecksum from "./pages/ValidateChecksum";
+import Combinatorics from "./pages/Combinatorics";
 import ThemeToggle from "./components/ThemeToggle";
 import LoadingScreen from "./components/LoadingScreen";
 import "./styles/App.css";
@@ -40,13 +42,15 @@ const App = () => {
           <div className="container">
             <Routes>
               <Route path="/" element={<Home />} />
-              <Route path="/cnf_to_dnf" element={<div>CNF to DNF Page</div>} />
+              {/* <Route path="/cnf_to_dnf" element={<div>CNF to DNF Page</div>} />
               <Route path="/recurrence_relations" element={<RecurrenceRelations />} />
               <Route path="/solving_recurrence_relations" element={<SolvingRecurrenceRelations />} />
-              <Route path="/number_theory" element={<div>Number Theory Page</div>} />
+              <Route path="/number_theory" element={<div>Number Theory Page</div>} /> */}
               <Route path="/sets" element={<Sets />} />
               <Route path="/decrypt" element={<DecryptMessage />} />
               <Route path="/hamming" element={<HammingCode />} />
+              <Route path="/validate_checksum" element={<ValidateChecksum />} />
+              <Route path="/combinatorics" element={<Combinatorics />} />
             </Routes>
           </div>
         </Router>
